25ce02c8ff07e4bc52e63dec9da4c680a1b49639
MarcusEstr <estrada.marcus@gmail.com>
2018-10-27 19:01:47 -0700

    Search page Links back to root page via close button.

--- src/components/pages/SearchPage.js
+++ src/components/pages/SearchPage.js
@@ -1,9 +1,24 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 class SearchPage extends React.Component {
   render() {
     return (
-      <p>Search Page</p>
+      <div className="search-books">
+        <div className="search-books-bar">
+          <Link to="/" className="close-search">Close</Link>
+          <div className="search-books-input-wrapper">
+            {
+
+            }
+            <input type="text" placeholder="Search by title or author"/>
+
+          </div>
+        </div>
+        <div className="search-books-results">
+          <ol className="books-grid"></ol>
+        </div>
+      </div>
     );
   }
 }
