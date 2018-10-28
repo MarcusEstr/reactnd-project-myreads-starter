c39188a05e21b12057d1bca3cef2623d3c0c5ef4
MarcusEstr <estrada.marcus@gmail.com>
2018-10-27 19:45:46 -0700

    Get shelves rendered on root page via components.

--- src/components/pages/MainPage.js
+++ src/components/pages/MainPage.js
@@ -1,8 +1,24 @@
 import React from 'react'
+import * as BooksAPI from '../../BooksAPI'
 import { Link } from 'react-router-dom'
 import Shelf from '../Shelf';
 
 class MainPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      books: []
+    }
+  }
+
+  componentDidMount() {
+    BooksAPI.getAll()
+    .then(resp => {
+      console.log(resp);
+      this.setState({ books: resp});
+    });
+  }
+
   render() {
     return (
       <div className="list-books">
@@ -10,7 +26,9 @@ class MainPage extends React.Component {
           <h1>MyReads</h1>
         </div>
         <div className="list-books-content">
-          <Shelf />
+          <Shelf name="Currently Reading" books={this.state.books.filter(b => b.shelf === "currentlyReading")}/>
+          <Shelf name="Want to Read" books={this.state.books.filter(b => b.shelf === "wantToRead")}/>
+          <Shelf name="Read" books={this.state.books.filter(b => b.shelf === "read")}/>
         </div>
         <div className="open-search">
           <Link to="/search">Add a book</Link>
