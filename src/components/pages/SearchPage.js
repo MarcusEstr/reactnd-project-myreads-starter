a6025725598a5818544e633d35e6d90644fea13d
MarcusEstr <estrada.marcus@gmail.com>
2018-10-27 20:34:17 -0700

    Allow books to be removed from shelves.

--- src/components/pages/SearchPage.js
+++ src/components/pages/SearchPage.js
@@ -1,7 +1,35 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import * as BooksAPI from '../../BooksAPI'
 
 class SearchPage extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      books: [],
+      results: []
+    }
+  }
+
+  componentDidMount() {
+    BooksAPI.getAll()
+    .then(resp => {
+      console.log(resp);
+      this.setState({ books: resp});
+    });
+  }
+  
+  updateBook = (book, shelf) => {
+    BooksAPI.update(book, shelf)
+    .then(resp => {
+      book.shelf = shelf;
+      this.setState(state => ({
+        books: state.books.filter(b => b.id !== book.id).concat([book])
+      }));
+    });
+  }
+
   render() {
     return (
       <div className="search-books">
